24f5749d35602e5a24d9226e07fc28f6ebdee25d
Akashleena <akash13leena@gmail.com>
2020-11-16 23:17:00 +0530

    Launching a KInova Jaco using a Gazebo Plugin
    The urdf file was converted to sdf file
    and a Gazebo Plugin was written to launch the arm

---
+++ build/CMakeFiles/GazeboPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GazeboPlugin.dir/src/GazeboPlugin.cpp.o"
+  "libGazeboPlugin.pdb"
+  "libGazeboPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GazeboPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
